fc10ec10cf53ea458b4339fd8001bb79309be993
naruesorn-krabkhuntod <naruesorn.krabkhuntod@outlook.com>
2023-12-23 17:28:58 +0700

    add home page

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const debug = require('debug')('app');
 const morgan = require('morgan')
+const path = require('path')
 const app = express();
 const port = 1080;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.get("/", (req,res) =>{
+
+app.get("/", (req, res) => {
     res.send("hhhhh");
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     debug('[node]' + ' listening' + port);
 })
